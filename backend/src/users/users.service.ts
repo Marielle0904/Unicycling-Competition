39eecdddf745bc04418a6f69c229640027195804
Marielle <Marielle.Klein0904@gmx.de>
2026-09-06 18:04:05 +0200

    Hash passwords during registration

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) { }
 
   async register(data: RegisterUserDto) {
-    return this.prisma.user.create({
+    const passwordHash = await argon2.hash(data.password);
+
+    const user = await this.prisma.user.create({
       data: {
         email: data.email,
-        password: data.password,
+        password: passwordHash,
         firstName: data.firstName,
         lastName: data.lastName,
         birthDate: new Date(data.birthDate),
       },
     });
+
+    const { password, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
   }
 }
