5d923e4bded44cc1a671f430ddaecc098b5c9485
Marielle <Marielle.Klein0904@gmx.de>
2026-09-06 12:12:29 +0200

    Implement user registration endpoint

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -4,4 +4,22 @@ import { PrismaService } from '../prisma/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) { }
+
+  async register(data: {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+  }) {
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: data.password,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        birthDate: new Date(data.birthDate),
+      },
+    });
+  }
 }
